683bd54d5837c984475e8d1b456f5f8741d4a500
NadiaQN <quezada.nadia@gmail.com>
2018-07-30 12:14:07 -0400

    sincronizando

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -20,69 +20,71 @@ function closeMenu() {
 
 // CAMARAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
-(function() {
-  var streaming = false,
-    video = document.querySelector("#video"),
-    canvas = document.querySelector("#canvas"),
-    photo = document.querySelector("#photo"),
-    startbutton = document.querySelector("#startbutton"),
-    width = 320,
-    height = 0;
+/*
+    Tomar una fotografía y guardarla en un archivo
+    @date 2017-11-22
+    @author parzibyte
+    @web parzibyte.me/blog
+*/
+function tieneSoporteUserMedia() {
+    return !!(navigator.getUserMedia || (navigator.mozGetUserMedia || navigator.mediaDevices.getUserMedia) || navigator.webkitGetUserMedia || navigator.msGetUserMedia)
+}
 
-  navigator.getMedia =
-    navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia;
+function _getUserMedia() {
+    return (navigator.getUserMedia || (navigator.mozGetUserMedia || navigator.mediaDevices.getUserMedia) || navigator.webkitGetUserMedia || navigator.msGetUserMedia).apply(navigator, arguments);
+}
 
-  navigator.getMedia(
-    {
-      video: true,
-      audio: false
-    },
-    function(stream) {
-      if (navigator.mozGetUserMedia) {
-        video.mozSrcObject = stream;
-      } else {
-        var vendorURL = window.URL || window.webkitURL;
-        video.src = vendorURL.createObjectURL(stream);
-      }
-      video.play();
-    },
-    function(err) {
-      console.log("An error occured! " + err);
-    }
-  );
+// Declaramos elementos del DOM
+var $video = document.getElementById("video"),
+    $canvas = document.getElementById("canvas"),
+    $boton = document.getElementById("boton"),
+    $estado = document.getElementById("estado");
+if (tieneSoporteUserMedia()) {
+    _getUserMedia({
+            video: true
+        },
+        function(stream) {
+            console.log("Permiso concedido");
+            $video.srcObject = stream;
+            $video.play();
 
-  video.addEventListener(
-    "canplay",
-    function(ev) {
-      if (!streaming) {
-        height = video.videoHeight / (video.videoWidth / width);
-        video.setAttribute("width", width);
-        video.setAttribute("height", height);
-        canvas.setAttribute("width", width);
-        canvas.setAttribute("height", height);
-        streaming = true;
-      }
-    },false );
+            //Escuchar el click
+            $boton.addEventListener("click", function() {
 
-  function takepicture() {
-    canvas.width = width;
-    canvas.height = height;
-    canvas.getContext("2d").drawImage(video, 0, 0, width, height);
-    var data = canvas.toDataURL("image/png");
-    photo.setAttribute("src", data);
-  }
+                //Pausar reproducción
+                $video.pause();
 
-  startbutton.addEventListener(
-    "click",
-    function(ev) {
-      takepicture();
-      ev.preventDefault();
-    },
-    false
-  );
-})();
+                //Obtener contexto del canvas y dibujar sobre él
+                var contexto = $canvas.getContext("2d");
+                $canvas.width = $video.videoWidth;
+                $canvas.height = $video.videoHeight;
+                contexto.drawImage($video, 0, 0, $canvas.width, $canvas.height);
 
-// CAMARAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                var foto = $canvas.toDataURL(); //Esta es la foto, en base 64
+                $estado.innerHTML = "Enviando foto. Por favor, espera...";
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "./guardar_foto.php", true);
+                xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                xhr.send(encodeURIComponent(foto)); //Codificar y enviar
+
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+                        console.log("La foto fue enviada correctamente");
+                        console.log(xhr);
+                        $estado.innerHTML = "Foto guardada con éxito. Puedes verla <a target='_blank' href='./" + xhr.responseText + "'> aquí</a>";
+                    }
+                }
+
+                //Reanudar reproducción
+                $video.play();
+            });
+        },
+        function(error) {
+            console.log("Permiso denegado o error: ", error);
+            $estado.innerHTML = "No se puede acceder a la cámara, o no diste permiso.";
+        });
+} else {
+    alert("Lo siento. Tu navegador no soporta esta característica");
+    $estado.innerHTML = "Parece que tu navegador no soporta esta característica. Intenta actualizarlo.";
+}
+    // CAMARAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
