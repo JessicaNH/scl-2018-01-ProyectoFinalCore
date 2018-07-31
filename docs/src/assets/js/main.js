0075057ac6b6e5167e3ffc141c1de836da8f1600
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-07-31 17:58:44 -0400

    solucionado extracto de función repetida (firebase)

--- docs/src/assets/js/main.js
+++ docs/src/assets/js/main.js
@@ -74,28 +74,6 @@ infoVisit.addEventListener('click', event => {
     .once('value', function datosIf(send) {
       tblUsersList.innerHTML = ''; // se evita la repeticion de la visita
 
-      firebase
-        .database()
-
-        .ref('/zonaIf')
-        .once('value', function datosIf(send) {
-          printInfoVisit.innerHTML = ' '; // se evita la repeticion de la visita
-
-          ref('/zonaIf')
-            .once('value', function datosIf(send) {
-              printInfoVisit.innerHTML = ' '; // se evita la repeticion de la visita 
-              Object.entries(send.val()).forEach(sends => {
-                printInfoVisit.innerHTML += `<div>
-            ${sends[1].rut}
-            ${sends[1].nombre}
-            ${sends[1].apellido}
-            ${sends[1].recinto}
-            ${sends[1].fecha}  
-            ${sends[1].hora}
-            <i class="fas fa-sign-out-alt" data-post="${sends[0]}" onclick="deletePost(event)"></i></div>`;
-              });
-            });
-        });
       // aqui evaluamos la ruta y se imprime en HTML
       firebase
         .database()
@@ -105,15 +83,12 @@ infoVisit.addEventListener('click', event => {
 
           Object.entries(send.val()).forEach(sends => {
             tblUsersList.innerHTML += `<tr>
-      <td>  ${sends[1].rut}</td>
+       <td>  ${sends[1].rut}</td>
        <td>   ${sends[1].nombre}</td>
        <td>   ${sends[1].apellido}</td>
        <td>   ${sends[1].recinto}</td>
        <td>   ${sends[1].fecha}  </td>
        <td>   ${sends[1].hora}</td>
-       <i class="fas fa-sign-out-alt" data-post="${
-  sends[0]
-}" onclick="deletePost(event)"></i>
     </tr>`; 
           });
         });
