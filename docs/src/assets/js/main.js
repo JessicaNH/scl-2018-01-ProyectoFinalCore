28ae4448032b67a6cac290e203f19137930689ff
NadiaQN <quezada.nadia@gmail.com>
2018-07-31 13:53:52 -0400

    resolviendo posibles confictos pequeños

--- docs/src/assets/js/main.js
+++ docs/src/assets/js/main.js
@@ -30,8 +30,8 @@ infoVisit.addEventListener('click', event => {
   var hour = formatoFecha.getHours();
   var min = formatoFecha.getMinutes();
 
-  fecha = day + "/" + month + "/" + year;
-  hora = hour + ":" + min;
+  fecha = day + '/' + month + '/' + year;
+  hora = hour + ':' + min;
   // Aqui se obtiene el valor del select
   let selectOptionsIf = document.getElementById('zonaIfOptions');
   selectOptionsIf.addEventListener('click', function() {
@@ -58,12 +58,12 @@ infoVisit.addEventListener('click', event => {
       .ref('zonaIf')
       .push(infoUsuarioIf) // Hacemos referencia el nombre del objeto que contendrá nuestros registros y empujamos los nuevos envios de datos
       .then(function() {
-        alert("Se ha enviado un aviso de su llegada"); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
+        alert('Se ha enviado un aviso de su llegada'); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
 
-        alert("mensaje guardado"); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
+        alert('mensaje guardado'); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
       })
       .catch(function() {
-        alert("No fue posible guardar su selección"); // En caso de ocurrir un error le mostramos al usuario que ocurrió un error.
+        alert('No fue posible guardar su selección'); // En caso de ocurrir un error le mostramos al usuario que ocurrió un error.
       });
   }
   // aqui evaluamos la ruta y se imprime en HTML
