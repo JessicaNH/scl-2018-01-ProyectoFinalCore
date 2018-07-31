82ddb8d0be687f3836b88c2b0502e29efe052656
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-07-31 16:31:27 -0400

    quité botón admin

--- docs/src/assets/js/app.js
+++ docs/src/assets/js/app.js
@@ -1,92 +1,3 @@
-function saveContactForm(infoUsuarioIf) {
-  firebase
-    .database()
-    .ref('zonaIf')
-    .push(infoUsuarioIf) // Hacemos referencia el nombre del objeto que contendrá nuestros registros y empujamos los nuevos envios de datos
-    .then(function() {
-      alert('Se ha enviado un aviso de su llegada'); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
-
-      alert('mensaje guardado'); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
-    })
-    .catch(function() {
-      alert('No fue posible guardar su selección'); // En caso de ocurrir un error le mostramos al usuario que ocurrió un error.
-    });
-}
-
-// aqui evaluamos la ruta y se imprime en HTML
-
-firebase.database().ref('/zonaIf')
-  .once('value', function datosIf(send) {
-    tblUsersList.innerHTML = ''; // se evita la repeticion de la visita
-
-    // aqui evaluamos la ruta y se imprime en HTML
-    firebase
-      .database()
-      .ref('/zonaIf')
-      .once('value', function datosIf(send) {
-        tblUsersList.innerHTML = ''; // se evita la repeticion de la visita
-
-        Object.entries(send.val()).forEach(sends => {
-          tblUsersList.innerHTML += `<tr>
-    <td>  ${sends[1].rut}</td>
-     <td>   ${sends[1].nombre}</td>
-     <td>   ${sends[1].apellido}</td>
-     <td>   ${sends[1].recinto}</td>
-     <td>   ${sends[1].fecha}  </td>
-     <td>   ${sends[1].hora}</td>
-     <i class="fas fa-sign-out-alt" data-post="${ 
-  sends[0]
-}" onclick="deletePost(event)"></i>
-  </tr>`; 
-        });
-      });
-  });
-
-function inicio() {
-  screenRegister.style.display = 'none';
-  screenStart.style.display = 'block';
-  screenVisit.style.display = 'none';
-  menuDesp.style.display = 'none';
-  closeMenu();
-}
-
-// registro de service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// pedir permiso para realizar notificaciones
-let messaging = firebase.messaging();
-messaging.requestPermission()
-  .then(function() {
-    console.log('Se han aceptado las notificaciones');
-    return messaging.getToken();
-  })
-  .the(function(token) {
-    if (token) { 
-      guardarToken(token);
-    } else {
-      anulaToken();
-    }
-  })
-  .catch(function(err) {
-    mensajeFeedback(err);
-    console.log('No se ha recibido permiso / token: ', err);
-  });
-messaging.onMessage(function(payload) {
-  console.log('Mensaje recibido con el sitio activo', payload);
-  mensajeFeedback(payload.notification.title + ': ' + payload.notification.body);
-});
-
-
 function toggleMenu() {
   // añadir función onclick="toggleMenu()" al botón del nav bar y al botón cerrar.
   if (sideMenu.className.indexOf('menu_closed') >= 0) {
@@ -122,3 +33,79 @@ function registrosTotales() {
   menuDesp.style.display = 'block';
   closeMenu();
 }
+
+function inicio() {
+  screenRegister.style.display = 'none';
+  screenStart.style.display = 'block';
+  screenVisit.style.display = 'none';
+  menuDesp.style.display = 'none';
+  closeMenu();
+}
+
+// CAMARA
+
+(function() {
+  var streaming = false,
+    video = document.querySelector('#video'),
+    canvas = document.querySelector('#canvas'),
+    photo = document.querySelector('#photo'),
+    startbutton = document.querySelector('#startbutton'),
+    width = 320,
+    height = 0;
+
+  navigator.getMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+
+  navigator.getMedia(
+    {
+      video: true,
+      audio: false
+    },
+    function(stream) {
+      if (navigator.mozGetUserMedia) {
+        video.mozSrcObject = stream;
+      } else {
+        var vendorURL = window.URL || window.webkitURL;
+        video.src = vendorURL.createObjectURL(stream);
+      }
+      video.play();
+    },
+    function(err) {
+      console.log('An error occured! ' + err);
+    }
+  );
+
+  video.addEventListener(
+    'canplay',
+    function(ev) {
+      if (!streaming) {
+        height = video.videoHeight / (video.videoWidth / width);
+        video.setAttribute('width', width);
+        video.setAttribute('height', height);
+        canvas.setAttribute('width', width);
+        canvas.setAttribute('height', height);
+        streaming = true;
+      }
+    }, false);
+});
+
+
+function takepicture() {
+  canvas.width = width;
+  canvas.height = height;
+  canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+  let data = canvas.toDataURL('image/png');
+  photo.setAttribute('src', data);
+}
+
+startbutton.addEventListener(
+  'click',
+  function(ev) {
+    takepicture();
+    ev.preventDefault();
+  },
+  false
+);
