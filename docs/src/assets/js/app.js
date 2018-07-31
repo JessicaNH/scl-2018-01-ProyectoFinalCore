d23e2d857af31f1600731e0ea3e7470d8f6c238a
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-07-31 01:04:33 -0400

    agregada función link inicio (cambiar a home?)

--- docs/src/assets/js/app.js
+++ docs/src/assets/js/app.js
@@ -34,6 +34,14 @@ function registrosTotales() {
   closeMenu();
 }
 
+function inicio() {
+  screenRegister.style.display = 'none';
+  screenStart.style.display = 'block';
+  screenVisit.style.display = 'none';
+  menuDesp.style.display = 'none';
+  closeMenu();
+}
+
 // CAMARA
 
 (function() {
